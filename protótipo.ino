49b8256f12359c0d6bc5d6549589d2482d8d3bd0
gbgodoy <gbgodoyk@gmail.com>
2019-11-13 10:04:10 -0300

    Update protótipo.ino

--- protótipo.ino
+++ protótipo.ino
@@ -1,167 +1,166 @@
-  #include <EEPROM.h>
-	#include <ESP8266WiFi.h>    
-  #include <WiFiClient.h> 
-  #include <ESP8266WebServer.h>
-  #include <ESP8266HTTPClient.h>
-  #include <SPI.h>
-  #include <MFRC522.h>     
-  #include <Wire.h>
-  #include <LiquidCrystal_I2C.h>
+#include <EEPROM.h>
+#include <Ultrasonic.h>             // SENSOR ULTRASONICO
+#include <SPI.h>
+#include <MFRC522.h>              //RFID     
+#include <Wire.h>               //LCD
+#include <LiquidCrystal_I2C.h>          //LCD
+#include <AccelStepper.h>           //movimento dos motores
 
-  #define SS_PIN 53
-  #define RST_PIN 49
-  #define pirPin 7
-  #define ledPin 3
-	#define but1Pin 23	//compra1
-	#define but1Pin 24	//compra2
-	#define but1Pin 25	//compra3
-	#define but1Pin 26	//compra4
-	#define but1Pin 27	//compra5
+#define SS_PIN 26               //pino envio RFID
+#define RST_PIN 27                //pino reset RFID
+#define relePin 28                //pino do relê
+#define trigPin 29                //pino do ultrasom
+#define echoPin 30                //pino do ultrasom
 
-	pinMode(trigPin, OUTPUT);
-	pinMode(echoPin, OUTPUT);
-	pinMode(relePin, OUTPUT);
-	pinMode(but1Pin, INPUT_PULLUP);
-	pinMode(but2Pin, INPUT_PULLUP);
-	pinMode(but3Pin, INPUT_PULLUP);
-	pinMode(but4Pin, INPUT_PULLUP);
-	pinMode(but5Pin, INPUT_PULLUP);
+#define led1Pin 33
+#define led2Pin 34
+#define led3Pin 37
+#define led4Pin 35
+#define led5Pin 36
 
-  MFRC522 mfrc522(SS_PIN, RST_PIN);
-  LiquidCrystal_I2C lcd(0x27, 16, 2);
+#define homeX 44                //início de curso eixo X
+#define homeY 45                //início de curso eixo Y
+#define endY 46                 //início de curso eixo Y
 
-  const char *ssid = "SSID";
-  const char *password = "password";
+#define dirPinX 48                //direção moto eixo X
+#define stepPinX 49   
+#define dirPinY1 50               //direção moto eixo Y
+#define stepPinY1 51    
+#define dirPinY2 52               //direção moto eixo Y
+#define stepPinY2 53    
 
-  const char *host = "192.168.x.x";
+const int selectButton01 = 38;          //botões
+const int selectButton02 = 39;          //botões
+const int selectButton03 = 40;          //botões
+const int selectButton04 = 41;          //botões
+const int selectButton05 = 42;          //botões
 
-  String getData ,Link;
-  String CardID="";
+#define motorInterfaceType 1
 
-  void setup() {
-  	mensageminicial();
-    delay(1000);
-    Serial.begin(115200);
-    SPI.begin();  // Init SPI bus
-    mfrc522.PCD_Init(); // Init MFRC522 card
+AccelStepper stepperX = AccelStepper(motorInterfaceType, stepPinX, dirPinX);
+AccelStepper stepperY1 = AccelStepper(motorInterfaceType, stepPinY1, dirPinY1);
+AccelStepper stepperY2 = AccelStepper(motorInterfaceType, stepPinY2, dirPinY2);
 
-    WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
-    delay(1000);
-    WiFi.mode(WIFI_STA);        //This line hides the viewing of ESP as wifi hotspot
-    
-    WiFi.begin(ssid, password);     //Connect to your WiFi router
-    Serial.println("");
+Ultrasonic sr04(trigPin, echoPin);
+MFRC522 mfrc522(SS_PIN, RST_PIN);       
+LiquidCrystal_I2C lcd(0x27, 16, 2);       
 
-    Serial.print("Connecting to ");
-    Serial.print(ssid);
-    // Wait for connection
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
+int distance = 0;
+bool activeSession = false;
+int engineProduct = 0;
+int activateDistance;
+
+void setup() {
+  digitalWrite(relePin,LOW);            //motor inicia desligado
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led1Pin, OUTPUT);
+  Serial.begin(9600);
+  mfrc522.PCD_Init();
+}
+
+void loop() {
+
+  distance = checkDistance();
+
+  if (!activeSession) {
+    if (distance <= activateDistance) {
+      greetings();
+      activeSession = true;
     }
-
-    //If connection successful show IP address in serial monitor
-    Serial.println("");
-    Serial.println("Connected");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());  //IP address assigned to your ESP
-
-    pinMode(RedLed,OUTPUT);
-    pinMode(BlueLed,OUTPUT);
   }
 
-  void loop() {
-    if(WiFi.status() != WL_CONNECTED){
-      WiFi.disconnect();
-      WiFi.mode(WIFI_STA);
-      Serial.print("Reconnecting to ");
-      Serial.println(ssid);
-      WiFi.begin(ssid, password);
-      while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-      }
-      Serial.println("");
-      Serial.println("Connected");
-      Serial.print("IP address: ");
-      Serial.println(WiFi.localIP());
-    }
-    
-    if ( mfrc522.PICC_IsNewCardPresent()) {
-      if ( mfrc522.PICC_ReadCardSerial()) {
-        Serial.print("UID da Tag:");
-        for (byte i = 0;i < mfrc522.uid.size; i++){
-          CardID += mfrc522.uid.uidByte[i];
-          Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-          Serial.print(mfrc522.uid.uidByte[i], HEX);
-        }
-        Serial.println();
-        mfrc522.PICC_HaltA();  
-      }
-    }
-    
-    HTTPClient http;
-    
-    //GET
-    getData = "?CardID=" + CardID;
-    Link = "http://www.euboton.com.br/envio.php" + getData;
-    
-    http.begin(Link);
-    
-    int httpCode = http.GET();            //Send the request
-    delay(10);
-    String payload = http.getString();    //Get the response payload
-    
-    Serial.println(httpCode);   //Print HTTP return code
-    Serial.println(payload);    //Print request response payload
-    Serial.println(CardID);     //Print Card ID
-    
-    if (payload == "liberado"){
-      nemas();
-      delay(500);  //Post Data at every 5 seconds
-    }
-    delay(500);
-    
-    CardID = "";
-    getData = "";
-    Link = "";
-    http.end();  //Close connection
-	}
+  if (activeSession) {
+      engineProduct = productSelection(0, 3, 7);
+      nichoSelecionado(engineProduct);
+      activeSession = false;
+      delay(2500);
 
-	void nemas() {
-		if (digitalRead(but1Pin)!=0) {
-			}	
-			while (digitalRead(but1Pin)) {
-				}
-		   	break;
-		if (digitalRead(but2Pin)!=0) {
-			}	
-			while (digitalRead(but2Pin)) {
-				}
-		   	break;
-		if (digitalRead(but3Pin)!=0) {
-			}	
-			while (digitalRead(but3Pin)) {
-				}
-		   	break;
-		if (digitalRead(but4Pin)!=0) {
-			}	
-			while (digitalRead(but4Pin)) {
-				}
-		   	break;
-		if (digitalRead(but4Pin)!=0) {
-			}	
-			while (digitalRead(but4Pin)) {
-				}
-		   	break;
-		}
-	
-	void mensageminicial(){
-	  lcd.init(); 
-	  lcd.backlight();
-	  lcd.home();
-	  lcd.setCursor(4, 0);
-	  lcd.print(-=LUCINA=-);
-	  delay(3000);
-	  lcd.backlight(LOW);
-	}
+  }
+
+}
+
+int checkDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  return sr04.Ranging(CM);
+  delay(500);
+} 
+
+void greetings() {
+  for (int i = 0; i < 255; i += 5) {
+    analogWrite(led1Pin, i);
+    delay(30);
+  }
+  for (int i = 255; i > 122; i--) {
+    analogWrite(led1Pin, i);
+    delay(10);
+  }
+}
+
+int productSelection(int button, int initialButton, int finalButton) {
+  while (button == 0) {
+   for (int i = initialButton; i < finalButton; i++) {
+     if (digitalRead(i) == HIGH) {
+       button = i;
+       return i;
+     }
+   }
+  }
+}
+
+int nichoSelecionado(int engineProduct) {
+
+  switch (engineProduct) {
+
+      case 1:
+        stepperX.moveTo(100);
+        stepperX.runToPosition();
+          break;
+
+      case 2:
+        stepperX.moveTo(200);
+        stepperX.runToPosition();
+        break;
+
+      case 3:
+        stepperX.moveTo(300);
+        stepperX.runToPosition();
+        break;
+
+      case 4:
+          stepperX.moveTo(400);
+      stepperX.runToPosition();
+      break;
+
+      case 5:
+        stepperX.moveTo(500);
+      stepperX.runToPosition();
+        break;
+  }
+}
+
+String validaCartao() { 
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+  Serial.print("Validando Cartão:");
+  String conteudo = "";
+  byte letra;
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    conteudo.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Mensagem : ");
+  conteudo.toUpperCase();
+  return conteudo;
+}
